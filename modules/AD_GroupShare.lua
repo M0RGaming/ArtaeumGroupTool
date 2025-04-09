8c1376b4141696fb91263312576a90ab64b176ce
Chinmay Patil <chinmaytlc@gmail.com>
2025-04-09 11:34:16 -0600

    Update protocols, add initial support for hammer, ping, and camp lock protocols

--- modules/AD_GroupShare.lua
+++ modules/AD_GroupShare.lua
@@ -109,41 +109,23 @@ function group.init()
 		handler:SetDisplayName("Artaeum Group Tool")
 		handler:SetDescription("Group Tool for Coordination in Cyrodiil!")
 
-		-- protocols to insert: assist Ping, camp Lock, hammer bar, ult bar
 
 		group.protocols = {}
 
-		group.protocols.ping = handler:DeclareProtocol(500, "ArtaeumPing") -- only send on button press
+		-- between 3 and 3 bytes
+		group.protocols.ping = handler:DeclareProtocol(90, "ArtaeumPing") -- only send on button press
 		group.protocols.ping:AddField(LGB.CreateFlagField("ping"))
 		group.protocols.ping:OnData(group.handlers.onPing)
 		group.protocols.ping:Finalize({isRelevantInCombat = true})
 
-		group.protocols.lock = handler:DeclareProtocol(501, "ArtaeumCampLock") -- only send when camp updates to be locked or unlocked
-		group.protocols.lock:AddField(LGB.CreateFlagField("lock"))
-		group.protocols.lock:OnData(group.handlers.onCampLock)
-		group.protocols.lock:Finalize({isRelevantInCombat = true})
-
-		group.protocols.hammerBar = handler:DeclareProtocol(502, "ArtaeumDaedricPower") -- only send when holding artifact, if not holding say 0%
-		group.protocols.hammerBar:AddField(LGB.CreatePercentageField("percent")) -- should be between 0 and 1
-		group.protocols.hammerBar:OnData(group.handlers.onHammerUpdate)
-		group.protocols.hammerBar:Finalize({isRelevantInCombat = true})
-
-		group.protocols.requestSync = handler:DeclareProtocol(503, "ArtaeumRequestSync") -- run when joining a group to obtain info
-		group.protocols.requestSync:AddField(LGB.CreateFlagField("requested"))
-		group.protocols.requestSync:OnData(group.handlers.onSyncRequested)
-		group.protocols.requestSync:Finalize()
-
-		group.protocols.sync = handler:DeclareProtocol(504, "ArtaeumSync")
-		group.protocols.sync:AddField(LGB.CreateFlagField("lock"))
-		group.protocols.sync:AddField(LGB.CreatePercentageField("percent"))
-		group.protocols.sync:AddField(LGB.CreateFlagField("shareFB")) -- share Front Bar
+		-- between 3 and 4 bytes
+		group.protocols.sync = handler:DeclareProtocol(91, "ArtaeumDataSync")
+		group.protocols.sync:AddField(LGB.CreateOptionalField(LGB.CreateFlagField("lock"))) -- ArtaeumCampLock (501), only when lock updates on/off
+		group.protocols.sync:AddField(LGB.CreateOptionalField(LGB.CreatePercentageField("hammer"))) -- ArtaeumDaedricPower (502), only when holding
+		group.protocols.sync:AddField(LGB.CreateOptionalField(LGB.CreateFlagField("requestSync"))) -- ArtaeumRequestSync (503), request resend
 		group.protocols.sync:OnData(group.handlers.onSync)
-		group.protocols.sync:Finalize()
+		group.protocols.sync:Finalize({isRelevantInCombat = true, replaceQueuedMessages = false})
 
-		group.protocols.sharingBar = handler:DeclareProtocol(505, "ArtaeumSharingBar") -- Might remove, idk
-		group.protocols.sharingBar:AddField(LGB.CreateFlagField("shareFB"))
-		group.protocols.sharingBar:OnData(group.handlers.onSharingBarUpdate)
-		group.protocols.sharingBar:Finalize()
 
 
 		local GroupResources = LibGroupBroadcast:GetHandlerApi("GroupResources")
@@ -201,7 +183,6 @@ local directions = {
 }
 
 function group.requestAssistPing()
-	group.toSend.assistPing = true
 	local playerX,playerY = GetMapPlayerPosition('player')
 	local crownX,crownY = GetMapPlayerPosition(GetGroupLeaderUnitTag())
 	local dir = math.atan2(playerX-crownX,crownY-playerY)/math.pi
@@ -217,10 +198,15 @@ function group.requestAssistPing()
 	if direction ~= "" then
 		d("You are "..direction.." of crown.")
 	end
+
+	group.protocols.ping:Send({
+		ping = true
+	})
+	--group.handlers.onPing('player', {ping=true})
+	-- TODO: maybe display ping locally too
 end
 
 function group.ping()
-	group.toSend:send()
 end
 
 
@@ -277,47 +263,51 @@ function group.handlers.onCampLock(unitTag, data)
 	end
 end
 
+
+local function lgcsUpdate(unitTag)
+	local realData = group.lgcs:GetUnitULT(unitTag)
+	group.lgcsCallback(unitTag, realData)
+end
+
 local hammerWeilder = ''
 function group.handlers.onHammerUpdate(unitTag, data)
-	--data.percent
-	-- idk if the handler comes back if the player is the one who sent. I'm assuming no, but ill put a check anyways
-	if AreUnitsEqual(unitTag,'player') then d("Artaeum: PLAYER GOT PING") end
-	if (data.percent == 0) then
+	--data.hammer
+	-- player also gets their own messages
+	if AreUnitsEqual(unitTag,'player') then
+		lgcsUpdate()
+		return
+	end
+
+	if (data.hammer == 0) then
 		if (hammerWeilder == unitTag) then
 			if not HUD_DAEDRIC_ENERGY_METER:IsHidden() then HUD_DAEDRIC_ENERGY_METER:UpdateVisibility() end
 			hammerWeilder = ''
 		end
 	else
-		if (hammerWeilder == unitTag) then
-			if HUD_DAEDRIC_ENERGY_METER:IsHidden() then
-				HUD_DAEDRIC_ENERGY_METER:SetHiddenForReason("daedricArtifactInactive",false,SHOULD_FADE_OUT)
-			end
-			HUD_DAEDRIC_ENERGY_METER:UpdateEnergyValues(data.percent,1)
-		else
-			hammerWeilder = unitTag
+		hammerWeilder = unitTag
+		if HUD_DAEDRIC_ENERGY_METER:IsHidden() then
+			HUD_DAEDRIC_ENERGY_METER:SetHiddenForReason("daedricArtifactInactive",false,SHOULD_FADE_OUT)
 		end
+		HUD_DAEDRIC_ENERGY_METER:UpdateEnergyValues(data.hammer,1)
 	end
-end
-
-function group.handlers.onSyncRequested(unitTag, data)
-	--data.requested
-	if data.requested then
-		-- send Sync
-	end
+	lgcsUpdate()
 end
 
 function group.handlers.onSync(unitTag, data)
+	--a = data
 	--data.lock
-	--data.percent
-	--data.shareFB
-	group.handlers.onCampLock(unitTag, data)
-	group.handlers.onHammerUpdate(unitTag, data)
-	group.handlers.onSharingBarUpdate(unitTag, data)
-end
-
-function group.handlers.onSharingBarUpdate(unitTag, data)
-	--data.shareFB
-	-- TODO: DO THIS ENTIRE FUNCTION
+	--data.hammer
+	--data.requestSync
+	if data.lock ~= nil then
+		group.handlers.onCampLock(unitTag, data)
+	end
+	if data.hammer ~= nil then
+		group.handlers.onHammerUpdate(unitTag, data)
+	end
+	if data.requestSync == true then
+		-- send data
+		group.sync()
+	end
 end
 
 function group.handlers.onStamUpdate(unitTag, unitName, current, max, percent)
@@ -332,6 +322,59 @@ function group.handlers.onMagUpdate(unitTag, unitName, current, max, percent)
 		if frameDB[unitTag].magStamHidden then frameDB[unitTag]:SetMagStamHidden(false) end
 		frameDB[unitTag]:SetMag(percent,1)
 	end
+end
+
+
+
+--group.protocols.ping
+--group.protocols.sync
+
+-- EVENT_FORWARD_CAMP_RESPAWN_TIMER_BEGINS
+-- EVENT_POWER_UPDATE - add filter for POWERTYPE_DAEDRIC
+
+
+-- on player activated:
+--[[
+update locally:
+current camp lock (GetNextForwardCampRespawnTime() > GetGameTimeMilliseconds())
+
+send:
+current camp lock (GetNextForwardCampRespawnTime() > GetGameTimeMilliseconds())
+hammer bar (GetUnitPower('player',POWERTYPE_DAEDRIC))
+requestSync = true
+
+
+/script ArtaeumGroupTool.Group.send(true,1,false)
+/script ArtaeumGroupTool.Group.send(nil,nil,true)
+]]
+
+
+function group.send(campLock, hammerBar, requestSync)
+	group.protocols.sync:Send({
+		lock = campLock,
+		hammer = hammerBar,
+		requestSync = requestSync
+	})
+end
+
+function group.sendHammer(_, unit, powerIndex, powerType, current, max)
+	if max == 0 then max = 1 end
+	local hammerBar = current/max
+	group.send(nil, hammerBar, nil)
+end
+
+function group.sendLock()
+	-- TODO: ALL OF THIS
+end
+
+function group.sync(requestSync)
+	local hammerCurrent, hammerMax = GetUnitPower('player',POWERTYPE_DAEDRIC)
+	if hammerMax == 0 then hammerMax = 1 end
+	local hammerBar = hammerCurrent/hammerMax
+
+	local campLock = GetNextForwardCampRespawnTime() > GetGameTimeMilliseconds()
+
+	group.send(campLock, hammerBar, requestSync)
 end
 
 
@@ -357,29 +400,7 @@ function group.toSend:send()
 	local stamBar = math.floor(stamCurrent/stamMax*15)
 --]]
 	local xstream = {0,campLock,assistPing,hammerBar,ult.id,0}
-	local ystream = {ult.percent,0,magBar,stamBar}
 
-
-	local x = group.writeStream(
-		xstream,
-		{1,1,1,4,8,1}
-	)
-	local y = group.writeStream(
-		ystream,
-		{7,1,4,4}
-	)
-	LGPS:PushCurrentMap()
-	if not LMP:IsPingSuppressed(MAP_PIN_TYPE_PING) then
-		LMP:SuppressPing(MAP_PIN_TYPE_PING)
-	end
-	SetMapToMapId(group.mapID)
-	LMP:SetMapPing(
-		MAP_PIN_TYPE_PING,
-		MAP_TYPE_LOCATION_CENTERED,
-		x*group.stepSize,
-		y*group.stepSize
-	)
-	LGPS:PopCurrentMap()
 end
 
 
@@ -406,10 +427,20 @@ end
 
 group.lgcsCallbackDebug = {}
 
-local ultIconLookup = {}
+local ultIconLookup = {
+	[0] = "/esoui/art/icons/heraldrycrests_misc_blank_01.dds", -- no ult
+	[116096] = "/esoui/art/icons/ability_artifact_volendrung_006.dds", -- hammer ult
+}
 function group.lgcsCallback(unitTag, data)
 	group.lgcsCallbackDebug[unitTag] = data --- DEBUG STUFF
 	
+	if hammerWeilder == unitTag then -- if unit has hammer
+		data.ult1Cost = 250
+		data.ult2Cost = 250
+		data.ult1ID = 116096
+		data.ult2ID = 116096
+	end
+
 	local ult1Id = data.ult1ID
 	local ult1Icon = ultIconLookup[ult1Id]
 	if ult1Icon == nil then
@@ -423,6 +454,9 @@ function group.lgcsCallback(unitTag, data)
 		ult2Icon = GetAbilityIcon(ult2Id)
 		ultIconLookup[ult2Id] = ult2Icon
 	end
+
+
+
 
 	frameDB[unitTag]:setUlt(data.ultValue, data.ult1Cost, ult1Icon, data.ult2Cost, ult2Icon)
 	frameDB[unitTag].image:SetColor(1,1,1)
@@ -556,6 +590,8 @@ function group.scaleWindow()
 		toplevels[i]:SetScale(scale)
 	end
 end
+
+-- TODO: Make all additional windows achor to window 1
 
 
 function group.showWindows()
@@ -696,7 +732,7 @@ function group.updateSharing(sharing)
 		EVENT_MANAGER:UnregisterForEvent("AD Group Tool Group Connect", EVENT_GROUP_MEMBER_CONNECTED_STATUS)
 		EVENT_MANAGER:UnregisterForEvent("AD Group Tool Group Range", EVENT_GROUP_SUPPORT_RANGE_UPDATE)
 		EVENT_MANAGER:UnregisterForEvent("AD Group Tool Group Role", EVENT_GROUP_MEMBER_ROLE_CHANGED)
-		EVENT_MANAGER:UnregisterForEvent("AD Group Tool Group Frames Activated", EVENT_PLAYER_ACTIVATED)
+		EVENT_MANAGER:UnregisterForEvent("AD Group Tool Group Daedric Power", EVENT_POWER_UPDATE)
 		
 
 		if vars.UI == "Custom" then
@@ -724,7 +760,9 @@ function group.updateSharing(sharing)
 		EVENT_MANAGER:RegisterForEvent("AD Group Tool Group Connect", EVENT_GROUP_MEMBER_CONNECTED_STATUS, group.updateOnline)
 		EVENT_MANAGER:RegisterForEvent("AD Group Tool Group Range", EVENT_GROUP_SUPPORT_RANGE_UPDATE, group.updateRange)
 		EVENT_MANAGER:RegisterForEvent("AD Group Tool Group Role", EVENT_GROUP_MEMBER_ROLE_CHANGED, group.updateRole)
-		EVENT_MANAGER:RegisterForEvent("AD Group Tool Group Frames Activated", EVENT_PLAYER_ACTIVATED, group.groupUpdate)
+		EVENT_MANAGER:RegisterForEvent("AD Group Tool Group Daedric Power", EVENT_POWER_UPDATE, group.sendHammer)
+		EVENT_MANAGER:AddFilterForEvent("AD Group Tool Group Daedric Power", EVENT_POWER_UPDATE, REGISTER_FILTER_POWER_TYPE, COMBAT_MECHANIC_FLAGS_DAEDRIC)
+		EVENT_MANAGER:AddFilterForEvent("AD Group Tool Group Daedric Power", EVENT_POWER_UPDATE, REGISTER_FILTER_UNIT_TAG, "player")
 		
 		
 		--if IsUnitGrouped("player") then
@@ -740,9 +778,11 @@ function group.updateSharing(sharing)
 		group.running = true
 		group.groupLeadChange()
 		group.groupUpdate()
+		group.sync(true)
 	elseif group.running and sharing then
 		group.groupLeadChange()
 		group.groupUpdate()
+		group.sync(true)
 	end
 
 end
@@ -761,191 +801,3 @@ end
 ZO_CreateStringId("SI_BINDING_NAME_ARTAEUMGROUPTOOL_REQUEST_PING", "Send a assist ping.")
 SLASH_COMMANDS["/ping"] = group.ping
 
-
-
-
-group.ultLookup = {
-
-}
-group.ultiIndexes = {
-	[1] = 15957, -- Magma Armor
-	[2] = 16536, -- Meteor
-	[3] = 17874, -- Magma Shell
-	[4] = 17878, -- Corrosive Armor
-	[5] = 20671, -- Molten Fury
-	[6] = 20679, -- Blood Fury
-	[7] = 20689, -- Controlled Fury
-	[8] = 21752, -- Nova
-	[9] = 21755, -- Solar Prison
-	[10] = 21758, -- Solar Disturbance
-	[11] = 22138, -- Radial Sweep
-	[12] = 22139, -- Crescent Sweep
-	[13] = 22144, -- Everlasting Sweep
-	[14] = 22223, -- Rite of Passage
-	[15] = 22226, -- Practiced Incantation
-	[16] = 22229, -- Remembrance
-	[17] = 23492, -- Greater Storm Atronach
-	[18] = 23495, -- Summon Charged Atronach
-	[19] = 23634, -- Summon Storm Atronach
-	[20] = 24785, -- Overload
-	[21] = 24804, -- Energy Overload
-	[22] = 24806, -- Power Overload
-	[23] = 25091, -- Soul Shred
-	[24] = 25411, -- Consuming Darkness
-	[25] = 26598, -- Arm Wabbajack
-	[26] = 27706, -- Negate Magic
-	[27] = 28341, -- Suppression Field
-	[28] = 28348, -- Absorption Field
-	[29] = 28988, -- Dragonknight Standard
-	[30] = 29012, -- Dragon Leap
-	[31] = 32455, -- Werewolf Transformation
-	[32] = 32624, -- Blood Scion
-	[33] = 32715, -- Ferocious Leap
-	[34] = 32719, -- Take Flight
-	[35] = 32947, -- Standard of Might
-	[36] = 32958, -- Shifting Standard
-	[37] = 32963, -- Shift Standard
-	[38] = 33398, -- Death Stroke
-	[39] = 35460, -- Soul Tether
-	[40] = 35508, -- Soul Siphon
-	[41] = 35713, -- Dawnbreaker
-	[42] = 36485, -- Veil of Blades
-	[43] = 36493, -- Bolstering Darkness
-	[44] = 36508, -- Incapacitating Strike
-	[45] = 36514, -- Soul Harvest
-	[46] = 38563, -- War Horn
-	[47] = 38573, -- Barrier
-	[48] = 38931, -- Perfect Scion
-	[49] = 38932, -- Swarming Scion
-	[50] = 39075, -- Pack Leader
-	[51] = 39076, -- Werewolf Berserker
-	[52] = 39270, -- Soul Strike
-	[53] = 40158, -- Dawnbreaker of Smiting
-	[54] = 40161, -- Flawless Dawnbreaker
-	[55] = 40220, -- Sturdy Horn
-	[56] = 40223, -- Aggressive Horn
-	[57] = 40237, -- Reviving Barrier
-	[58] = 40239, -- Replenishing Barrier
-	[59] = 40414, -- Shatter Soul
-	[60] = 40420, -- Soul Assault
-	[61] = 40489, -- Ice Comet
-	[62] = 40493, -- Shooting Star
-	[63] = 49886, -- Impenetrable Ward
-	[64] = 49899, -- Lightning Assault
-	[65] = 50303, -- Legendary Heal Other
-	[66] = 50385, -- Rapid Recovery
-	[67] = 50468, -- Drain Soul
-	[68] = 50501, -- Cataclysm
-	[69] = 50544, -- Ice Armor
-	[70] = 50570, -- Hypothermia
-	[71] = 50605, -- Blood Thirsty Familiar
-	[72] = 50663, -- Ultimate Flame Atronach
-	[73] = 50790, -- Conjure Dremora Ruler
-	[74] = 50872, -- Ebonyflesh
-	[75] = 50898, -- Magicka Invulnerability
-	[76] = 50961, -- Mass Paralysis
-	[77] = 50981, -- Encumber
-	[78] = 51016, -- Heroic Courage
-	[79] = 51153, -- Hushed Feet
-	[80] = 51248, -- Incite Frenzy
-	[81] = 52897, -- Standard of Might
-	[82] = 53875, -- Heroic Courage
-	[83] = 54118, -- Remembrance
-	[84] = 55090, -- Devouring Swarm
-	[85] = 61090, -- Standard of Might
-	[86] = 79064, -- Veil of Blades
-	[87] = 83216, -- Berserker Strike
-	[88] = 83229, -- Onslaught
-	[89] = 83238, -- Berserker Rage
-	[90] = 83272, -- Shield Wall
-	[91] = 83292, -- Spell Wall
-	[92] = 83310, -- Shield Discipline
-	[93] = 83465, -- Rapid Fire
-	[94] = 83552, -- Panacea
-	[95] = 83600, -- Lacerate
-	[96] = 83619, -- Elemental Storm
-	[97] = 83625, -- Fire Storm
-	[98] = 83628, -- Ice Storm
-	[99] = 83630, -- Thunder Storm
-	[100] = 83642, -- Eye of the Storm
-	[101] = 83682, -- Eye of Flame
-	[102] = 83684, -- Eye of Frost
-	[103] = 83686, -- Eye of Lightning
-	[104] = 83850, -- Life Giver
-	[105] = 83867, -- Live Giver
-	[106] = 84434, -- Elemental Rage
-	[107] = 85126, -- Fiery Rage
-	[108] = 85128, -- Icy Rage
-	[109] = 85130, -- Thunderous Rage
-	[110] = 85132, -- Light's Champion
-	[111] = 85156, -- Lacerate
-	[112] = 85179, -- Thrive in Chaos
-	[113] = 85187, -- Rend
-	[114] = 85257, -- Toxic Barrage
-	[115] = 85451, -- Ballista
-	[116] = 85532, -- Secluded Grove
-	[117] = 85804, -- Enchanted Forest
-	[118] = 85807, -- Healing Thicket
-	[119] = 85982, -- Feral Guardian
-	[120] = 85986, -- Eternal Guardian
-	[121] = 85990, -- Wild Guardian
-	[122] = 86109, -- Sleet Storm
-	[123] = 86113, -- Northern Storm
-	[124] = 86117, -- Permafrost
-	[125] = 88158, -- Materialize
-	[126] = 90284, -- Guardian's Wrath
-	[127] = 92163, -- Guardian's Savagery
-	[128] = 94625, -- Guardian's Wrath
-	[129] = 103478, -- Undo
-	[130] = 103557, -- Precognition
-	[131] = 103564, -- Temporal Guard
-	[132] = 113105, -- Incapacitating Strike
-	[133] = 113505, -- Discharge Energy
-	[134] = 115001, -- Bone Goliath Transformation
-	[135] = 115361, -- Shock Field
-	[136] = 115410, -- Reanimate
-	[137] = 116096, -- Ruinous Cyclone
-	[138] = 118279, -- Ravenous Goliath
-	[139] = 118367, -- Renewing Animation
-	[140] = 118379, -- Animate Blastbones
-	[141] = 118664, -- Pummeling Goliath
-	[142] = 122174, -- Frozen Colossus
-	[143] = 122388, -- Glacial Colossus
-	[144] = 122395, -- Pestilent Colossus
-	[145] = 122908, -- Super Pummeling Goliath
-	[146] = 126489, -- Berserker Strike
-	[147] = 126492, -- Berserker Rage
-	[148] = 126497, -- Onslaught
-	[149] = 129375, -- Vampire Lord
-	[150] = 133507, -- Lead the Pack
-	[151] = 157016, -- Unleashed Rage
-	[152] = 157259, -- Impeccable Shot
-	[153] = 160715, -- Scurry
-	[154] = 163763, -- Baneslayer
-	[155] = 164191, -- Raging Storm
-	[156] = 164413, -- Channel the Storm
-	[157] = 164881, -- Overcharge
-	[158] = 165208, -- Unstable Shield
-	[159] = 165281, -- Arcane Wards
-	[160] = 165412, -- Cyclone
-	[161] = 165810, -- Spell Conversion
-	[162] = 165875, -- Forced Sacrifice
-	[163] = 166037, -- Time Tear
-	[164] = 183676, -- Gibbering Shield
-	[165] = 183709, -- Vitalizing Glyphic
-	[166] = 186488, -- Gore
-	[167] = 189791, -- The Unblinking Eye
-	[168] = 189837, -- The Tide King's Gaze
-	[169] = 189867, -- The Languid Eye
-	[170] = 192372, -- Sanctum of the Abyssal Sea
-	[171] = 192380, -- Gibbering Shelter
-	[172] = 193558, -- Resonating Glyphic
-	[173] = 193794, -- Glyphic of the Tides
-	[174] = 195103, -- Vigorous Tentacular Eruption
-	[175] = 196782, -- Rite of Passage
-}
-group.ultList = {}
-for k,v in pairs(group.ultiIndexes) do
-	group.ultList[v] = GetAbilityIcon(v)
-	group.ultiIndexes[v]=k
-end
